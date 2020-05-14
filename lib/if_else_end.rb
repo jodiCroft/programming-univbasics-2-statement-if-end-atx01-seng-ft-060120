4ec532ee7f8d4e14ae3afb9d545f83ebdfb07daa
Jodi Croft <jodipruett@gmail.com>
2020-05-14 21:35:09 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,12 @@
-if Time.now.to_i % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
+#if Time.now.to_i % 2 == 0
+#  puts "Even!"
+#else
+#  puts "Odd!"
+#end
+
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
 end
+puts "Code after if...end"
